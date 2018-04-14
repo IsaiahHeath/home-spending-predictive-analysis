c82f28ff3f3ded7935c1e00aeae974288ae1cce3
Hassan Rahim Kamil <rahimka2@illinois.edu>
2018-04-14 17:48:11 +0000

    Plot validation data (sales on date)

--- R codes.R
+++ R codes.R
@@ -1,3 +1,35 @@
+library("ggplot2")
+library("stringr")
+library("dplyr")
+library("magrittr")
+library("tidyr")
+library("ggthemes")
 
+#read into R
 validation = read.csv("Validation.csv")
 development = read.csv("Development.csv")
+
+
+#changing column names
+colnames(validation) = c("date", "sale")
+colnames(development) = c("date", "sale")
+
+#changing sale to numeric
+development$sale = str_extract(development$sale, pattern = "[^$]+") %>% 
+  str_replace(pattern = ",", replacement = "") %>%
+  as.numeric()
+ 
+#changing date to d/m/YYYY format 
+year = str_extract(development$date, pattern = "[:digit:]{2}$") 
+year = ifelse(str_detect(year, pattern = "9[0-9]") == TRUE, paste0("19", year), paste0("20", year))
+month = str_extract(development$date, pattern = "(?<=/)[:digit:]+(?=/)")
+day = str_extract(development$date, pattern = "^[:digit:]+(?=/)")
+development$date = paste(day, month, year, sep = "/")
+
+ggplot(development, aes(x = as.Date(development$date, format = "%m/%d/%Y"), y = sale)) + 
+  geom_line(aes(color = "yellow")) + labs(x = "date") + 
+  theme_hc() + 
+  theme(legend.position = "none")
+
+
+
